e118daad84cf4ed16365648bd274475d2b386db9
shiguojing <shiguojing@wejoydata.com>
2023-04-26 13:58:54 +0800

    opt code

--- src/routes/IndexRoute.tsx
+++ src/routes/IndexRoute.tsx
@@ -22,43 +22,12 @@ export function IndexRoute() {
     <>
       <Center py="xl" sx={{ height: "100%" }}>
         <Container size="sm">
-          <Badge mb="lg">GPT-4 Ready</Badge>
-
-          <Text mt={4} size="xl">
-            Dichatgpt
-          </Text>
-          <SimpleGrid
-            mt={50}
-            cols={3}
-            spacing={30}
-            breakpoints={[{ maxWidth: "md", cols: 1 }]}
-          >
-            {features.map((feature) => (
-              <div key={feature.title}>
-                <ThemeIcon variant="outline" size={50} radius={50}>
-                  <feature.icon size={26} stroke={1.5} />
-                </ThemeIcon>
-                <Text mt="sm" mb={7}>
-                  {feature.title}
-                </Text>
-                <Text size="sm" color="dimmed" sx={{ lineHeight: 1.6 }}>
-                  {feature.description}
-                </Text>
-              </div>
-            ))}
-          </SimpleGrid>
+          <Badge mb="xs" color="teal" variant="outline">Dichatgpt</Badge>
         </Container>
       </Center>
     </>
   );
 }
 
-const features = [
-  {
-    icon: IconCurrencyDollar,
-    title: "Free and open source",
-    description:
-      "This app is provided for free and the source code is available on GitHub.",
-  },
 
-];
+
